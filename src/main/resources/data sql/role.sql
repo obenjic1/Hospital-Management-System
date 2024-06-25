cbf9190e8714e7578b214fd3632cdf09b017a1a2
vincent <vincent@presprintmail.cm>
2024-06-25 15:15:58 +0100

    task controlSheet prices validation

--- src/main/resources/data sql/role.sql
+++ src/main/resources/data sql/role.sql
@@ -1,23 +1,24 @@
 
 
-INSERT INTO `role`(`description`, `name`) VALUES ("Listing users in the system refers to obtaining a list of all user accounts that have been created on a particular operating system","ROLE_LIST_USERS");
-INSERT INTO `role`(`description`, `name`) VALUES ("Viewing user details in the system means accessing and examining the specific information associated with a user account","ROLE_VIEW_USER_DETAILS");
-INSERT INTO `role`(`description`, `name`) VALUES ("Updating users in the system refers to modifying or changing the attributes and settings associated with existing user accounts","ROLE_UPDATE_USER");
-INSERT INTO `role`(`description`, `name`) VALUES ("Removing users from the system refers to the process of deleting or disabling user accounts that are no longer needed or required","ROLE_REMOVE_USER");
-INSERT INTO `role`(`description`, `name`) VALUES ("Adding a group to the system means creating a new group account that can be used to the operating system.","ROLE_ADD_GROUP");
-INSERT INTO `role`(`description`, `name`) VALUES ("Listing group in the system refers to obtaining a list of all groups that have been created on a particular operating system","ROLE_LIST_GROUPS");
-INSERT INTO `role`(`description`, `name`) VALUES ("Viewing group details in the system means accessing and examining the specific information associated with a group","ROLE_VIEW_GROUP_DETAILS");
-INSERT INTO `role`(`description`, `name`) VALUES ("Updating group in the system refers to modifying or changing the attributes and settings associated with existing group","ROLE_UPDATE_GROUP");
-INSERT INTO `role`(`description`, `name`) VALUES ("block group from the system refers to the process of denied or disabling all right access to this group","ROLE_DENIED_GROUP");
-INSERT INTO `role`(`description`, `name`) VALUES ("Listing roles in the system refers to obtaining a list of all roles that have been created on our system","ROLE_LIST_ROLES");
-INSERT INTO `role`(`description`, `name`) VALUES ("Viewing role details in the system means accessing and examining the specific information associated with a group","ROLE_VIEW_ROLE_DETAILS");
-INSERT INTO `role`(`description`, `name`) VALUES ("Adding a customer to the system means creating a new customer account that can be used to the operating system","ROLE_SAVE_CUSTOMER");
-INSERT INTO `role`(`description`, `name`) VALUES ("Listing customer in the system refers to obtaining a list of all customer that have been created on our system","ROLE_LIST_CUSTOMER");
-INSERT INTO `role`(`description`, `name`) VALUES ("Viewing customer details in the system means accessing and examining the specific information associated with a customer","ROLE_VIEW_CUSTOMER_DETAILS");
-INSERT INTO `role`(`description`, `name`) VALUES ("Remove customer means that you'll delete all the customer informations and producs relate with him","ROLE_DELETE_CUSTOMER");
-INSERT INTO `role`(`description`, `name`) VALUES ("Updating customer in the system refers to modifying or changing the attributes and settings associated with existing customer","ROLE_UPDATE_CUSTOMER");
-INSERT INTO `role`(`description`, `name`) VALUES ("Manage setings means that you have the rigth access to add, view, delete and update everything have rapport with setings","ROLE_MANAGE_SETINGS");
-INSERT INTO `role`(`description`, `name`) VALUES ("View setting means that you can only view and select the settings element you cannot do the others operations.","ROLE_VIEW_SETINGS");
-INSERT INTO `role`(`description`, `name`) VALUES ("Default role to manage users","ROLE_MANAGE_USERS");
+INSERT INTO `role` (`id`, `created_at`, `description`, `name`) VALUES
+(1, NULL, 'Listing users in the system refers to obtaining a list of all user accounts that have been created on a particular operating system', 'ROLE_LIST_USERS'),
+(2, NULL, 'Viewing user details in the system means accessing and examining the specific information associated with a user account', 'ROLE_VIEW_USER_DETAILS'),
+(3, NULL, 'Updating users in the system refers to modifying or changing the attributes and settings associated with existing user accounts', 'ROLE_UPDATE_USER'),
+(4, NULL, 'Removing users from the system refers to the process of deleting or disabling user accounts that are no longer needed or required', 'ROLE_REMOVE_USER'),
+(5, NULL, 'Adding a group to the system means creating a new group account that can be used to the operating system.', 'ROLE_ADD_GROUP'),
+(6, NULL, 'Listing group in the system refers to obtaining a list of all groups that have been created on a particular operating system', 'ROLE_LIST_GROUPS'),
+(7, NULL, 'Viewing group details in the system means accessing and examining the specific information associated with a group', 'ROLE_VIEW_GROUP_DETAILS'),
+(8, NULL, 'Updating group in the system refers to modifying or changing the attributes and settings associated with existing group', 'ROLE_UPDATE_GROUP'),
+(9, NULL, 'block group from the system refers to the process of denied or disabling all right access to this group', 'ROLE_DENIED_GROUP'),
+(10, NULL, 'Listing roles in the system refers to obtaining a list of all roles that have been created on our system', 'ROLE_LIST_ROLES'),
+(11, NULL, 'Viewing role details in the system means accessing and examining the specific information associated with a group', 'ROLE_VIEW_ROLE_DETAILS'),
+(12, NULL, 'Adding a customer to the system means creating a new customer account that can be used to the operating system', 'ROLE_SAVE_CUSTOMER'),
+(13, NULL, 'Listing customer in the system refers to obtaining a list of all customer that have been created on our system', 'ROLE_LIST_CUSTOMER'),
+(14, NULL, 'Viewing customer details in the system means accessing and examining the specific information associated with a customer', 'ROLE_VIEW_CUSTOMER_DETAILS'),
+(15, NULL, 'Remove customer means that you''ll delete all the customer informations and producs relate with him', 'ROLE_DELETE_CUSTOMER'),
+(16, NULL, 'Updating customer in the system refers to modifying or changing the attributes and settings associated with existing customer', 'ROLE_UPDATE_CUSTOMER'),
+(17, NULL, 'Manage setings means that you have the rigth access to add, view, delete and update everything have rapport with setings', 'ROLE_MANAGE_SETINGS'),
+(18, NULL, 'View setting means that you can only view and select the settings element you cannot do the others operations.', 'ROLE_VIEW_SETINGS'),
+(19, NULL, 'Default role to manage users', 'ROLE_ADD_USER');
 
 
