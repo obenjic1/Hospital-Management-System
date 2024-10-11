d9db5f32c63600ecc67f399a505a7d7c044d6260
radius <radius@presprint.cm>
2024-10-11 13:06:12 +0100

    try add service worker

--- src/main/java/com/ppp/user/controller/authController.java
+++ src/main/java/com/ppp/user/controller/authController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
 import com.ppp.user.model.User;
@@ -70,4 +71,12 @@ public class authController {
 		
 			return "user_auth/resetpassword";
 		}
+		
+		  //Public page
+	    @RequestMapping(value = "/sw.js")
+	    public String serveWorker() {
+
+	        return "sw";
+	    }
+
 }
